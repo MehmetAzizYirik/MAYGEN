eb3a710486d085d1634d273c89733c3aefb9ceb6
Valentyn Kolesnikov <javadev75@gmail.com>
2022-01-26 07:32:56 +0200

    Fixed imports.

--- src/test/java/maygen/MaygenConsoleTest.java
+++ src/test/java/maygen/MaygenConsoleTest.java
@@ -21,9 +21,14 @@
 
 package maygen;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-import org.apache.commons.cli.*;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.junit.Test;
 
 /**
